5130330a822056f6db471146659238ede6e05a02
Peter Mansell <peter@pmansell.com>
2020-06-10 10:13:13 +0100

    - Removing all the old files

--- src/routes/index.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import APIRoutes from './api';
-let router = express.Router();
-
-router.use('/api', APIRoutes);
-
-export default router;
--- src/schemas/index.js
+++
@@ -1,5 +0,0 @@
-import Post from '../models/post';
-
-export default {
-    '/posts': Post.Schema,
-};
