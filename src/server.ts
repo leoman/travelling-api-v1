b146d436eec163cd136aec92c29d2fc374246ef8
Peter Mansell <peter@pmansell.com>
2020-06-16 09:29:48 +0100

    - Remove unwanted code from server file

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,6 @@ import { ApolloServer, makeExecutableSchema } from 'apollo-server'
 
 import logger from './logging'
 
-// import jwt from 'jsonwebtoken'
-
 import { postTypeDef, postResolver } from './posts'
 import { locationTypeDef } from './locations'
 import { photoTypeDef, photoResolver } from './photos'
@@ -14,33 +12,19 @@ import { sequelizeCheck, sequelizeSync } from './database'
 sequelizeCheck()
 sequelizeSync(false)
 
-// const JWT_SECRET = 'MY_SUPER_SECRET_KEY'
 
 const Query = `
   type Query {
     _empty: String,
-    hello: String
   }
   type Mutation {
     _empty: String,
   }
 `;
 
-interface Resolvers {
-  Query: {
-    [field: string]: (parent: any, args: object) => string;
-  }
-}
-
-const resolvers: Resolvers = {
-  Query: {
-    hello: () => 'Hello world!',
-  },
-}
-
 const schema = makeExecutableSchema({
   typeDefs: [ Query, postTypeDef, locationTypeDef, photoTypeDef, authTypeDef ],
-  resolvers: merge(resolvers, postResolver, photoResolver, authResolver),
+  resolvers: merge(postResolver, photoResolver, authResolver),
 })
 
 const server = new ApolloServer({ schema, context: async ({ req }) => authenticateToken(req) })
